c449875d866bc86b87feafbe2a775c09ec46d784
Vadim Semenov <gosusty@mail.ru>
2018-11-20 23:35:23 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -37,7 +37,7 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div className="card">
+            <div className='Site'>
                 <div className="card-header">
                     <div className='d-flex justify-content-between align-items-center'>
                         <h5 className='font-weight-bold'>Header</h5>
@@ -52,36 +52,39 @@ export default class App extends React.Component {
                         </button>
                     </div>
                 </div>
-                <div className='row'>
-                    {this.state.toggle &&
-                    <div className='col-4 border'>
-                        <p className='text-center'>Left</p>
-                        <p className='text-center'>Sidebar</p>
-                    </div>
-                    }
-                    <div className={this.state.toggle ? "col-8 border" : "col-12 border"}>
-                        <div className='container mt-2 d-flex justify-content-between align-items-center'>
-                            <button
-                                type='button'
-                                onClick={this.startRandom}
-                                className='btn btn-success btn-sm'
-                            >
-                                Force Rand
-                            </button>
-                            <div>
-                                Random Number : {this.state.randomNumber}
+                <div className='content'>
+                    <div className='row'>
+                        {this.state.toggle &&
+                        <div className='col-4 border'>
+                            <p className='text-center'>Left Sidebar</p>
+                            <p className='text-center'>Left Sidebar</p>
+                            <p className='text-center'>Left Sidebar</p>
+                        </div>
+                        }
+                        <div className={this.state.toggle ? "col-8" : "col-12"}>
+                            <div className='container mt-2 d-flex justify-content-between align-items-center'>
+                                <button
+                                    type='button'
+                                    onClick={this.startRandom}
+                                    className='btn btn-success btn-sm'
+                                >
+                                    Force Rand
+                                </button>
+                                <div>
+                                    Random Number : {this.state.randomNumber}
+                                </div>
+                                <button
+                                    type='button'
+                                    onClick={this.stopRandom}
+                                    className='btn btn-dark btn-sm'
+                                >
+                                    Stop
+                                </button>
                             </div>
-                            <button
-                                type='button'
-                                onClick={this.stopRandom}
-                                className='btn btn-dark btn-sm'
-                            >
-                                Stop
-                            </button>
                         </div>
                     </div>
                 </div>
-                <div className="card-footer text-muted">
+                <div className="card-footer text-muted footer">
                     <h5 className='font-weight-bold'>Footer</h5>
                 </div>
             </div>
